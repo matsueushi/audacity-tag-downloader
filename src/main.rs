bb1b9740be9840ef43fba9b1def4f70168e39051
matsueushi <sinxcosxtanx@gmail.com>
2018-10-27 13:16:58 -0400

    parse_release_info

--- src/main.rs
+++ src/main.rs
@@ -23,19 +23,23 @@ fn trim_artist(artist_str: &str) -> String {
     mat["artist"].to_string()
 }
 
+fn parse_release_info(release: discogs::data_structures::Release) {
+    println!("YEAR: {}", release.year);
+    println!("GENRE: {}", release.genres.unwrap().pop().unwrap());
+    println!(
+        "ARTIST: {}",
+        trim_artist(&release.artists.unwrap().pop().unwrap().name)
+    );
+    println!("ALBUM: {}", release.title);
+    println!("COUNTRY: {}", release.country.unwrap());
+    println!("TRACKLIST: {:?}", release.tracklist);
+}
+
 fn release_info(client: &mut Discogs, release_id: u32) {
-    let release = client.release(release_id).get();
-    match release {
-        Ok(release_result) => {
-            println!("YEAR: {}", release_result.year);
-            println!("GENRE: {}", release_result.genres.unwrap().pop().unwrap());
-            println!(
-                "ARTIST: {}",
-                trim_artist(&release_result.artists.unwrap().pop().unwrap().name)
-            );
-            println!("ALBUM: {}", release_result.title);
-            println!("COUNTRY: {}", release_result.country.unwrap());
-            println!("RELEASE: {:?}", release_result.tracklist);
+    let release_result = client.release(release_id).get();
+    match release_result {
+        Ok(release) => {
+            parse_release_info(release);
         }
         Err(_) => {
             println!("Release not found");
@@ -45,7 +49,6 @@ fn release_info(client: &mut Discogs, release_id: u32) {
 
 fn main() {
     let mut release_ids = env::args().map(|x| parse_release_id(&x)).skip(1);
-    println!("{:?}", release_ids.next());
     println!("{:?}", release_ids.next());
     println!("{:?}", release_ids.next());
 
@@ -80,6 +83,47 @@ mod tests {
         assert_eq!(trim_artist("Artist"), "Artist");
         assert_eq!(trim_artist("Artist (2)"), "Artist");
         assert_eq!(trim_artist("Artist (100)"), "Artist");
+    }
+
+    #[test]
+    fn parse_release_info_test() {
+        use discogs::data_structures::*;
+        let mut release = Release::new(
+            1234,
+            "title".to_string(),
+            "release".to_string(),
+            "released_formatted".to_string(),
+            "release_resource_url".to_string(),
+            "date_added".to_string(),
+            "date_changed".to_string(),
+            "url".to_string(),
+            2000,
+            vec![Artist::new(
+                123,
+                "artist_name".to_string(),
+                "artist_resource_url".to_string(),
+            )],
+            Status::Accepted,
+        );
+        release.genres = Some(vec!["genre".to_string()]);
+        release.country = Some("Japan".to_string());
+        release.tracklist = Some(vec![
+            Track {
+                duration: "1:00".to_string(),
+                position: "1".to_string(),
+                title: "title1".to_string(),
+                type_: "type".to_string(),
+                extra_artists: None,
+            },
+            Track {
+                duration: "3:00".to_string(),
+                position: "2".to_string(),
+                title: "title2".to_string(),
+                type_: "type".to_string(),
+                extra_artists: None,
+            },
+        ]);
+        parse_release_info(release);
     }
 
 }
